3257f7b3abb71397bb69e1cf60a984815c58d006
Korneliu <olbiton@gmail.com>
2020-12-06 22:41:37 -0800

    created Header component

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
+import Header from '../src/components/Header';
 
 const template = (
   <div>
-    <h1>Whatever</h1>
+    <Header />
   </div>
 )
 
