88960774a0125503bb1419cae30135bd2bd26778
Korneliu <olbiton@gmail.com>
2020-12-07 20:10:59 -0800

    added reducers folder with numbers, filters, notes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import Header from '../src/components/Header';
+import { createStore } from 'redux';
+
+const store = createStore = ((state = { count: 0 },) => {
+
+});
+
 
 const template = (
   <div>
