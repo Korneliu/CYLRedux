927778a9810821dd00d142b9bd9b3ebabcdfd44a
Korneliu <olbiton@gmail.com>
2020-12-07 23:11:04 -0800

    created store with increment, decrement, set and reset actions

--- src/app.js
+++ src/app.js
@@ -5,10 +5,59 @@ import './styles/styles.scss'
 import Header from '../src/components/Header';
 import { createStore } from 'redux';
 
-const store = createStore = ((state = { count: 0 },) => {
-
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+      return {
+        count: state.count + incrementBy
+      };
+    case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
+      return {
+        count: state.count - decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
+      }
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state
+  }
 });
 
+console.log(store.getState());
+
+store.subscribe(() => {
+  console.log(store.getState());
+})
+
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 5
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
+
+store.dispatch({
+  type: 'SET',
+  count: 101
+})
 
 const template = (
   <div>
